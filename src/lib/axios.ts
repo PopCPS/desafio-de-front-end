5be7493e283ed22706e190c1291565159ded7dc9
Artur Coelho <arturcoelho.cps@gmail.com>
2024-08-01 18:35:24 -0300

    updating apikey

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: `https://api.weatherapi.com/v1/forecast.json?key=${process.env.API_Key}&q=`
+  baseURL: `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_API_Key}&q=`
 })
